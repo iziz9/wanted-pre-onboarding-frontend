e4414e5b6ac6ead83002c4c3c1715a74f84f222b
iziz9 <46iz.us2@gmail.com>
2023-08-13 01:06:59 +0900

    Design: error 페이지 뒤로가기 버튼 추가

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { styled } from 'styled-components'
 
 const Error = () => {
-  return <Container>Error</Container>
+  const navigate = useNavigate()
+  return (
+    <Container>
+      <h1>Error</h1>
+      <h2>접근할 수 없는 페이지입니다.</h2>
+      <button onClick={() => navigate(-1)}>이전 페이지로</button>
+    </Container>
+  )
 }
 
 const Container = styled.main`
   position: relative;
+
+  h2 {
+    margin: 30px auto;
+  }
+
+  button {
+    width: 120px;
+    height: 40px;
+  }
 `
 
 export default Error
